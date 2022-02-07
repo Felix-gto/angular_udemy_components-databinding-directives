b0a350ce356725dccd5d38af46d7b2272208f402
Felix <felix.daraban@gmail.com>
2022-02-07 14:26:27 +0200

    Binding to Custom Events and assigning an Alias to Custom Event - Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
 
   // Declare Server element object: create an Test server example
   serverElements = [{type: 'server', name: 'Test server', content: 'Just a test!'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
